2850865053840a5c129c54c36f84c4c03bba5ea9
Satoru Nabeshima <snabeshi@chicagobooth.edu>
2023-05-07 15:32:20 +0000

    Random working perfectly

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get("/square/new",{:controller => "application", :action => "blank_square_form"})
+
   get("/square/results",{:controller => "application", :action => "calculate_square"})
+
+  get("/random/results",{:controller => "application", :action => "calculate_random"})
+
+  get("/random/new",{:controller => "application", :action => "blank_random_form"})
 end
